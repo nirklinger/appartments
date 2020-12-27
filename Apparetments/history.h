9201fea78c590e75d0a050dc831c0688805b53f0
Nir Klinger <nirkl@mta.ac.il>
2020-12-27 17:41:36 +0200

    History Files

--- Apparetments/history.h
+++ Apparetments/history.h
@@ -1,6 +1,7 @@
 #ifndef HISTORY
 #define HISTORY
 #include <stdbool.h>
+#include <stdio.h>
 
 typedef struct historyNode {
 	struct historyNode *next;
@@ -31,4 +32,16 @@ void shortTermHistory(int startIndex);
 int longTermHistory();
 
 char* getCommandFromHistory(char* f);
+
+char* replaceCommandStrings(char* commandToExecute, char* original, char* replace);
+
+void writeShortHistoryCommandsToFile(FILE* historyFile);
+
+void writeLongTermHistoryToFile(FILE *historyFile, HistoryNode *node);
+
+void writeHistoryFile();
+
+void loadHistoryFromoFile();
+
+void LoadNextCommand(FILE *historyFile, char* commandString);
 #endif //HISTORY 
